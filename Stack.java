d13b8819b0a6b19e416ab2bd983c6df8860f763d
Mohammad Hussain <mohammadhussainvadhaniya@gmail.com>
2024-09-18 09:00:46 +0530

    java program added

---
+++ Stack.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Stack
+{
+    public static void main(String[] args)
+    {
+        Stack st = new Stack();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+
+        System.out.println(st);
+    }
+}
--- Stringfunction.java
+++ Stringfunction.java
@@ -8,6 +8,11 @@ class Stringfunction
         s.trim();
         s.chatAt();
         s.length();
+        s.replace();
+        s.appends();
+        s.deletes();
+        s.reverses();
+        s.inserts();
     }
     public void lowerCase()
     {
@@ -34,15 +39,34 @@ class Stringfunction
         String s="  Mohammad Virat Kohli  ";    
         System.out.println("Length = "+s.length()); 
     }  
-    public static void main(String ar[])  
+    public void replace()  
     {  
         String s1="  Mohammad Virat Kohli  ";      
-        String replaceString=s1.replace("Mohammad","Ali");//replaces all occurrences of "Java" to "Kava"      
-        System.out.println(replaceString);   
+        String replaceString=s1.replace("Mohammad","Ali");      
+        System.out.println("Replacing Name = "+replaceString);   
     }
-    public static void main(String ar[])  
+    public void appends()
     {  
-        String s1="  Mohammad Virat Kohli  ";      
-        String replaceString=s1.replace("Mohammad","Ali");//replaces all occurrences of "Java" to "Kava"      
-        System.out.println(replaceString);   
+        StringBuffer sb=new StringBuffer(" Mohammad ");  
+        sb.append("Virat Kohli"); 
+        System.out.println("Concated String = "+sb);
     }
+    public void deletes()
+    {  
+        StringBuffer sb=new StringBuffer("Mohammad Virat Kohli");  
+        sb.delete(1,3);  
+        System.out.println("Deleted String = "+sb); 
+    }  
+    public void reverses()
+    {  
+        StringBuffer sb=new StringBuffer("Mohammad Virat Kohli");  
+        sb.reverse();  
+        System.out.println("Reversed String = "+sb); 
+    }  
+    public void inserts()
+    {  
+        StringBuffer sb=new StringBuffer("Mohammad ");  
+        sb.insert(9,"Virat Kohli"); 
+        System.out.println("After Inserting = "+sb);
+    }  
+}
